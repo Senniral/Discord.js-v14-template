86841bdf797e0ab6f3b3d0e5666b484a9be273bd
Senniral <60050784+Senniral@users.noreply.github.com>
2022-10-12 13:39:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,17 @@ const client = new Client({
 		GatewayIntentBits.GuildMembers,
 	],
 }); //all intents you might require.
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.once('ready', c => {
-	console.log(`Bot is ready, logged in as: ${c.user.tag}`);
-});
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 client.login(token);
