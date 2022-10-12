c11f0b068d3f5df3e1a1f4cae930d0066dd4f3f9
Senniral <60050784+Senniral@users.noreply.github.com>
2022-10-12 13:34:59 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+	],
+});
